33f52193e8b53a0a540be9d90df7d98288af42cb
nikhil <hp5cd1394q34@outlook.com>
2025-02-11 13:03:02 +0530

    Connection with MongoDB

--- src/index.js
+++ src/index.js
@@ -9,8 +9,16 @@ dotenv.config({
 })
 
 
-connectDB()
 
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`server is running at port:  ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("mongoDB connection failed !!!!! ", err);
+})
 
 
 
