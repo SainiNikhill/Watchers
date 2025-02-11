cb826ac9142e85f8efe9575ae1534638e07f72b5
nikhil <hp5cd1394q34@outlook.com>
2025-02-11 17:09:13 +0530

    added video and user model

--- src/app.js
+++ src/app.js
@@ -9,8 +9,12 @@ app.use(cors({
 }))
 
 app.use(express.json({limit: "20kb"}))
+app.use(express.urlencoded({extended:true, limit:"16kb"}))
+app.use(express.static("public"))
+
+app.use(cookieParser())
  
 
 
 
-export {app}
+export default app 
